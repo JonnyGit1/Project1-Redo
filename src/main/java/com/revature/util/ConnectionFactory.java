c80c79ab7d592f21cd7c59048b9a59b514decebf
Jonathan Ungaro <jonathanungaro@Jonathans-MacBook-Pro.local>
2019-04-25 20:32:39 -0400

    first real working example missing some

---
+++ src/main/java/com/revature/util/ConnectionFactory.java
@@ -0,0 +1,29 @@
+package com.revature.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+//  DB info here!---------------------------->>>>>>>>>
+	private static final String un = "jonathan";
+	private static final String pw = "password";
+	private static final String url = "jdbc:oracle:thin:@ersdbinstance.cclccpvm73o8.us-east-2.rds.amazonaws.com:1521:ORCL";
+	// Private Constructor to enforce usage of factory method
+	private ConnectionFactory() {}
+	
+	public static Connection getConnection() {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			return DriverManager.getConnection(url,un,pw);
+		} catch (SQLException e) {
+			System.err.println("Error Code: " + e.getErrorCode());
+			System.err.println("SQL State: " + e.getSQLState());
+			System.err.println("Message: " + e.getMessage());
+			throw new RuntimeException("Failed to get database connection");
+		} catch (ClassNotFoundException e) {
+			System.err.println("Failed to locate JDBC Driver");
+			throw new RuntimeException("Failed to locate JDBC driver");
+		}
+	}
+}
